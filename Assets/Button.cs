082532fa32348951235fc838791fcaa465a638cb
Dnk095 <darcnikill@gmail.com>
2024-08-22 03:16:09 +0300

    Update Button.cs

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -8,10 +8,7 @@ public class Button : MonoBehaviour
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("Button use");
             OnClick?.Invoke();
-        }
     }
 
 
