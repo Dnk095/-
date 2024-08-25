c48855e819ca6c4f2730b6b0a028abf6113449a1
Dnk095 <darcnikill@gmail.com>
2024-08-25 17:21:27 +0300

    Update Counter.cs

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -7,8 +7,10 @@ public class Counter : MonoBehaviour
     [SerializeField] private CLickActivator _clickButton;
 
     private Coroutine _coroutine;
+    private WaitForSeconds _wait;
     private int _currentCount = 0;
     private bool _isActive;
+    private float _delay=0.5f;
 
     public event Action CountChanged;
 
@@ -16,6 +18,7 @@ public class Counter : MonoBehaviour
 
     private void Start()
     {
+        _wait = new WaitForSeconds(_delay);
         _isActive = false;
     }
 
@@ -41,19 +44,17 @@ public class Counter : MonoBehaviour
         else
         {
             _isActive = true;
-            _coroutine = StartCoroutine(Countup(0.5f));
+            _coroutine = StartCoroutine(Countup());
         }
     }
 
-    private IEnumerator Countup(float delay)
+    private IEnumerator Countup()
     {
-        var wait = new WaitForSeconds(delay);
-
         while (true)
         {
             _currentCount++;
             CountChanged?.Invoke();
-            yield return wait;
+            yield return _wait;
         }
     }
 }
